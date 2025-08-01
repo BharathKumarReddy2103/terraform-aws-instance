5889801b07a5bf415115e0c70308b24f40b70e23
BharathKumarReddy <nbkumar2103@gmail.com>
2025-08-01 14:12:07 +0530

    updated file

--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@
     description = "AMI ID of the EC2 instance"
 }
 
+variable "subnet_id" {
+  type        = string
+  default = "subnet-027e9b95abbffe496"
+  description = "subnet ID of bharath RHEL9"
+}
+
 # EC2 instance type variable with validation for allowed values
 variable "instance_type" {
     default = "t3.micro"
