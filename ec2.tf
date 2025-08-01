d5e2ec302ffab6dc575b1258efe00fb4949e1667
BharathKumarReddy <nbkumar2103@gmail.com>
2025-08-01 14:36:43 +0530

    updated file

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "this" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   vpc_security_group_ids = var.sg_ids
+  associate_public_ip_address = true
 
   tags = var.tags
 }
